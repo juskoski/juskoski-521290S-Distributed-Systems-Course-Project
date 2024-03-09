ede6ea4ec4994ad792b1badcbb7901b671671e93
Jussi Koskinen <juskoski20@student.oulu.fi>
2024-03-09 11:37:44 +0200

    Add endpoint for creating secrets
    Added endpoint create-secret/ for adding new secrets. Updated
    INSTRUCTIONS.md to contain information about this endpoint.

--- secret_node/secret_node/urls.py
+++ secret_node/secret_node/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from secret_node_app.views import (
-    RequestAccessToSecret
+    RequestAccessToSecret,
+    CreateSecret,
 )
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('request-secret/', RequestAccessToSecret, name='request-secret'),
+    path('create-secret/', CreateSecret, name='create-secret'),
 ]
--- secret_node/secret_node_app/views.py
+++ secret_node/secret_node_app/views.py
@@ -6,7 +6,7 @@ from rest_framework import status
 import requests
 
 # Remember to start the Login node before running the following code
-VERIFY_TOKEN_URL = "http://localhost:8081/verify-token/"
+VERIFY_TOKEN_URL = "http://localhost:8000/verify-token/"
 
 
 @api_view(["POST"])
@@ -39,3 +39,39 @@ def RequestAccessToSecret(request: Request) -> Response:
 
     # Return the secret
     return Response({"secret": secret.secret}, status=status.HTTP_200_OK)
+
+
+@api_view (["POST"])
+def CreateSecret(request: Request) -> Response:
+    # Verify the request contains name of the secret and a valid token
+    if "secret_name" not in request.data or "access_token" not in request.data:
+        # Return an error if the request is missing required fields
+        return Response({"error": "Missing required fields"},
+                        status=status.HTTP_400_BAD_REQUEST)
+
+    # Verify the token is valid
+    token_data = {"access_token": request.data["access_token"]}
+    response = requests.post(VERIFY_TOKEN_URL, data=token_data)
+
+    # Return an error if the token is invalid
+    if response.status_code != 200:
+        return Response(
+            {"error": "Invalid token"}, status=status.HTTP_401_UNAUTHORIZED
+        )
+
+    # Check if the secret already exists
+    if SecretModel.objects.filter(name=request.data["secret_name"]).exists():
+        return Response(
+            {"error": "Secret already exists"},
+            status=status.HTTP_409_CONFLICT
+        )
+
+    # Create the secret
+    secret = SecretModel(name=request.data["secret_name"],
+                         secret=request.data["secret"])
+
+    # Save the secret to the database
+    secret.save()
+
+    # Return the secret
+    return Response({"secret": secret.secret}, status=status.HTTP_201_CREATED)
